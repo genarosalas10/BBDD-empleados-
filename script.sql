b0f8c64f6a82fd3129c022543dc513b154f64e3b
Genaro <Genarosalas10@gmail.com>
2021-10-14 10:12:24 +0200

    script para crear la base de datos y la tabla empleados

---
+++ script.sql
@@ -0,0 +1,11 @@
+create DATABASE empresa;
+  create table empleados
+CREATE TABLE  empleados(
+    idEmpleado tinyint UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	Dni char(9) NOT NULL  ,
+	Nombre varchar(60) NOT NULL,
+	Correo varchar(100) NULL,
+	Telefono char(9) NOT NULL,
+
+
+);
